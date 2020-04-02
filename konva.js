942b8ce5dfaa8a8ab2e072f3b7b8dbbac640414a
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-02 21:21:14 +0530

    added comment

--- konva.js
+++ konva.js
@@ -1,1 +1,7 @@
 //Stage
+//Layers are under stage
+//Layers1
+//LAyers2...upto n
+//shape 1 ,2 ,3 ..n under layer
+//than we have to add shape 2 to layer 1 or we can also add to anotehr layer
+//than we have to add layer to stage
