970363634827382d051212ccd2e559f21ad8d0fb
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-03 07:44:23 +0530

    added filter

--- konva.js
+++ konva.js
@@ -44,11 +44,15 @@ circle.cache();
 circle.filters([Konva.Filters,Blur]);
 circle.blurRadius(10);
 
+circle.cache();
+circle.filters([Konva.Filters,Blur]);
+circle.brightness(10);
+
 //events
 circle.on('click', function(){
 	this.blurRadius(0);
 	layer.draw();
-})
+});
 
 circle.on('mouseover', function(){
 	this.fill('black');
